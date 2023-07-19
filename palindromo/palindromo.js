dea2672ee56d9c3b7890a0161dd39cce2c20760a
Thiago Santos <91476713+Thmsantos@users.noreply.github.com>
2023-07-18 21:38:43 -0300

    Update palindromo.js

--- palindromo/palindromo.js
+++ palindromo/palindromo.js
@@ -9,12 +9,29 @@ leitor.question("Frase: ", function(answer) {
     var resp = answer;
     leitor.close();
     let array_padrao = resp.split("")
-    var array = []
+    let array = []
     for(i = 0; i < array_padrao.length; i++){
         if(array_padrao[i] != ' '){
             array.push(array_padrao[i])
         }
     }
-    console.log(array)
+   
+    let cont = 0
+
+    for(i = 0; i < array.length; i++){
+        let x = (array.length - 1) - i
+        
+        if(array[i] == array[x]){
+            cont = cont + 1
+        }else{
+            cont = cont - 1
+        }
+    }
+   
+    if(cont == array.length){
+        console.log('parabens, voce encontrou o palindromo perdido!')
+    }else{
+        console.log('a busca continua, o palindromo perdido ainda nao foi encontrado')
+    }
 });
 
